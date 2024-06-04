369a88ef3dc7cd49502bb74261bf1cac4f3432f8
alapierre-ops <drearyfying@outlook.fr>
2024-06-04 16:30:39 +0200

    Issue with declaration of users DAO

--- Backend/datamodel/seeder.js
+++ Backend/datamodel/seeder.js
@@ -7,7 +7,7 @@ module.exports = (usersService, interactionsService, messagesService, photosServ
     return new Promise(async (resolve, reject) => {
         try {
             await interactionsService.dao.db.query("CREATE TABLE IF NOT EXISTS interactions(id SERIAL PRIMARY KEY, date DATE, userWhoInteracted INTEGER, userShown INTEGER, liked BOOLEAN )")
-            await usersService.dao.db.query("CREATE TABLE IF NOT EXISTS users(id SERIAL PRIMARY KEY, nickname TEXT, email TEXT, password TEXT, bio TEXT, birthdate DATE, gender TEXT, tags TEXT, interestedIn TEXT, filter_agemin TEXT, filter_agemax TEXT, filter_dismax TEXT, city TEXT)")
+            await usersService.usersDAO.db.query("CREATE TABLE IF NOT EXISTS users(id SERIAL PRIMARY KEY, nickname TEXT, email TEXT, password TEXT, bio TEXT, birthdate DATE, gender TEXT, tags TEXT, interestedIn TEXT, filter_agemin TEXT, filter_agemax TEXT, filter_dismax TEXT, city TEXT)")
             await photosService.dao.db.query("CREATE TABLE IF NOT EXISTS photos(id SERIAL PRIMARY KEY, user_id TEXT, photo_data bytea )")
             await messagesService.dao.db.query("CREATE TABLE IF NOT EXISTS messages(id SERIAL PRIMARY KEY, sender_id TEXT, receiver_id TEXT, sent_at DATE, content TEXT )")
             resolve()
