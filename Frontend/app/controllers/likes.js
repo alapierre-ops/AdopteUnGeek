d3632e313de7599d67ec5436e2916b3328708b7e
alapierre-ops <drearyfying@outlook.fr>
2024-06-13 10:12:25 +0200

    Fixed a bug where clicking multiple times a category would show duplicates of the profiles

--- Frontend/app/controllers/likes.js
+++ Frontend/app/controllers/likes.js
@@ -7,6 +7,7 @@ class LikesController extends MainController {
         this.photosRoutes = new PhotosRoutes(this.apiUrl, this.token);
         this.interactionsRoutes = new InteractionsRoutes(this.apiUrl, this.token);
         this.initialize();
+        this.isLoading = false
     }
 
     async initialize(){
@@ -43,82 +44,87 @@ class LikesController extends MainController {
 
     async getUsers(category) {
         try {
-            const userGrid = document.getElementById('userGrid');
-            userGrid.innerHTML = '';
+            if(!this.isLoading) {
+                this.isLoading = true
+                const userGrid = document.getElementById('userGrid');
+                userGrid.innerHTML = '';
+                this.users = [];
 
-            if (category) {
-                document.getElementById("likedByOthers").classList.remove("category-option-selected")
-            }
-
-            if (!category || category === "likedMe") {
-                this.users = await this.interactionsRoutes.getLikedMe(this.userID);
-            }
-
-            if (category === "iLiked") {
-                this.users = await this.interactionsRoutes.getILiked(this.userID);
-            }
-
-            if (category === "matches") {
-                this.users = await this.interactionsRoutes.getMatches(this.userID);
-            }
-
-            console.log("getUsers(): ", this.users);
-
-            const usersPerRow = 3;
-
-            for (let i = 0; i < this.users.length; i += usersPerRow) {
-                const row = document.createElement('div');
-                row.classList.add('user-row');
-
-                for (let j = i; j < i + usersPerRow && j < this.users.length; j++) {
-                    const user = this.users[j];
-
-                    const personDiv = document.createElement('div');
-                    personDiv.classList.add('person');
-
-                    const imageContainer = document.createElement('div');
-                    imageContainer.classList.add('image-container');
-
-                    const img = document.createElement('img');
-                    img.alt = 'main picture';
-                    img.src = 'https://cdn.pixabay.com/animation/2023/11/18/09/26/09-26-08-99_512.gif';
-
-                    const actualImg = new Image();
-                    actualImg.src = await this.photosRoutes.getUserPhotos(user.id);
-                    actualImg.onload = () => {
-                        img.src = actualImg.src;
-                    };
-
-                    const textContainer = document.createElement('div');
-                    textContainer.classList.add('textContainer');
-
-                    const userFirstRow = document.createElement('div');
-                    userFirstRow.classList.add('userFirstRowLikes');
-
-                    const userName = document.createElement('h4');
-                    userName.textContent = user.nickname;
-
-                    const userAge = document.createElement('h4');
-                    const currentDate = new Date();
-                    const birthdate = new Date(user.birthdate);
-                    const differenceMs = currentDate - birthdate;
-                    const age = Math.floor(differenceMs / (1000 * 60 * 60 * 24 * 365));
-                    userAge.textContent = `${age} ans`;
-
-                    userFirstRow.appendChild(userName);
-                    userFirstRow.appendChild(userAge);
-                    textContainer.appendChild(userFirstRow);
-                    imageContainer.appendChild(img);
-                    imageContainer.appendChild(textContainer);
-                    personDiv.appendChild(imageContainer);
-                    row.appendChild(personDiv);
-
-                    personDiv.addEventListener('click', () => {
-                        window.location.href = `index.html?${user.id}`;
-                    });
+                if (category) {
+                    document.getElementById("likedByOthers").classList.remove("category-option-selected")
                 }
 
-                userGrid.appendChild(row);
+                if (!category || category === "likedMe") {
+                    this.users = await this.interactionsRoutes.getLikedMe(this.userID);
+                }
+
+                if (category === "iLiked") {
+                    this.users = await this.interactionsRoutes.getILiked(this.userID);
+                }
+
+                if (category === "matches") {
+                    this.users = await this.interactionsRoutes.getMatches(this.userID);
+                }
+
+                console.log("getUsers(): ", this.users);
+
+                const usersPerRow = 3;
+
+                for (let i = 0; i < this.users.length; i += usersPerRow) {
+                    const row = document.createElement('div');
+                    row.classList.add('user-row');
+
+                    for (let j = i; j < i + usersPerRow && j < this.users.length; j++) {
+                        const user = this.users[j];
+
+                        const personDiv = document.createElement('div');
+                        personDiv.classList.add('person');
+
+                        const imageContainer = document.createElement('div');
+                        imageContainer.classList.add('image-container');
+
+                        const img = document.createElement('img');
+                        img.alt = 'main picture';
+                        img.src = 'https://cdn.pixabay.com/animation/2023/11/18/09/26/09-26-08-99_512.gif';
+
+                        const actualImg = new Image();
+                        actualImg.src = await this.photosRoutes.getUserPhotos(user.id);
+                        actualImg.onload = () => {
+                            img.src = actualImg.src;
+                        };
+
+                        const textContainer = document.createElement('div');
+                        textContainer.classList.add('textContainer');
+
+                        const userFirstRow = document.createElement('div');
+                        userFirstRow.classList.add('userFirstRowLikes');
+
+                        const userName = document.createElement('h4');
+                        userName.textContent = user.nickname;
+
+                        const userAge = document.createElement('h4');
+                        const currentDate = new Date();
+                        const birthdate = new Date(user.birthdate);
+                        const differenceMs = currentDate - birthdate;
+                        const age = Math.floor(differenceMs / (1000 * 60 * 60 * 24 * 365));
+                        userAge.textContent = `${age} ans`;
+
+                        userFirstRow.appendChild(userName);
+                        userFirstRow.appendChild(userAge);
+                        textContainer.appendChild(userFirstRow);
+                        imageContainer.appendChild(img);
+                        imageContainer.appendChild(textContainer);
+                        personDiv.appendChild(imageContainer);
+                        row.appendChild(personDiv);
+
+                        personDiv.addEventListener('click', () => {
+                            window.location.href = `index.html?${user.id}`;
+                        });
+                    }
+
+                    userGrid.appendChild(row);
+                }
+                this.isLoading = false
             }
         } catch (error) {
             console.error('Error fetching users:', error);
